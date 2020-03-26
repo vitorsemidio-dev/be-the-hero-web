ef4a7b0e59f63d7848195b31e45d38692a9e61dc
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-03-26 20:45:25 -0300

    Remoção código

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import Logon from './pages/Logon';
 
 function App() {
-  let [counter, setCounter] = useState(0);
-
-  const increment = () => {
-    setCounter(counter + 1);
-    console.log(counter);
-  }
   return (
     <>
       <Logon/>
-
-      <button onClick={increment}>Incrementar</button>
     </>
   );
 }
