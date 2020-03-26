c74766edde45893b028f273b7e0fb30d40918b95
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-03-26 20:58:57 -0300

    Estrutura Logon

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -2,10 +2,29 @@ import React from 'react'
 
 import './styles.css';
 
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
+
 export default function Logon() {
   return (
-    <div>
-      Logon
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Logo"/>
+
+        <form action="">
+          <h1>Faça seu logon</h1>
+          
+          <input placeholder="Sua ID"/>
+          <button type="submit">Entrar</button>
+
+          <a href="/register">
+            Não tenho cadastro
+          </a>
+        </form>
+
+      </section>
+
+      <img src={heroesImg} alt="Heroes"/>
     </div>
   )
 }
