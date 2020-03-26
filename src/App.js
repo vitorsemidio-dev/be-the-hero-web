54a042578c1a8f92c95f06409610cdb40cb8a552
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-03-26 20:39:20 -0300

    Estado + Imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Header';
 
 function App() {
+  let [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+    console.log(counter);
+  }
   return (
     <>
       <Header title="Semana Omnistack" />
       <Header>
-        Semana Children
+        Contador: {counter}
       </Header>
+
+      <button onClick={increment}>Incrementar</button>
     </>
   );
 }
