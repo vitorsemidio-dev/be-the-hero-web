a2d05c19cdce9a3ec280e81d2c7f360fec817358
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-03-26 22:13:36 -0300

    Estrutura Profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPower } from 'react-icons/fi';
 
 import './styles.css';
 
+import logoImg from '../../assets/logo.svg';
+
 export default function Profile() {
   return (
-    <div>
-      Profile
+    <div className="profile-container">
+      <header>
+        <img src={logoImg} alt="Be The Hero"/>
+        <span>Bem vindo, Camp</span>
+
+        <Link className="button" to="/incidents/new">
+          Cadastrar Novo Caso
+        </Link>
+
+        <button type="button">
+          <FiPower size={18} color="#e02041" />
+        </button>
+      </header>
     </div>
   )
 }
