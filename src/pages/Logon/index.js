beff2a21c125f5f611dd246a6097cc927cdf5e24
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-03-27 00:18:18 -0300

    Redirecionamento e persistência no localStorage

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -12,12 +12,18 @@ import heroesImg from '../../assets/heroes.png';
 export default function Logon() {
   const [id, setId] = useState('');
 
+  const history = useHistory();
+
   const handleLogin = async e => {
     e.preventDefault();
 
     try {
       const response = await api.post('sessions', { id });
-      console.log(response.data.name);
+      
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+
+      history.push('/profile');
     } catch (err) {
       alert('Falha no login, tente novamente');
     }
