1ff248da317f215d7fbddd07c90abcde4e1f31c1
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-03-27 00:31:27 -0300

    Formatação moeda

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -51,7 +51,7 @@ export default function Profile() {
             <p>{incident.description}</p>
 
             <strong>VALOR:</strong>
-            <p>{incident.value}</p>
+            <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
 
             <button type="button">
               <FiTrash2 size={20}/>
