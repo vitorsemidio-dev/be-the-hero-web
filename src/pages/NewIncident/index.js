c46f1e671bfd489469dc37c3f3e743df1b4b153a
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-03-27 00:48:20 -0300

    Cadastro de novos casos

--- src/pages/NewIncident/index.js
+++ src/pages/NewIncident/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi'
 
+import api from '../../services/api';
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg'
 
 export default function NewIncident() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+
+  const ongId = localStorage.getItem('ongId');
+  const history = useHistory();
+
+  const handleNewIncident = async e => {
+    e.preventDefault();
+
+    const data = {
+      title, description, value,
+    };
+
+    try {
+      await api.post('incidents', data, {
+        headers: {
+          Authorization: ongId
+        }
+      });
+      history.push('/profile');
+    } catch (err) {
+      alert('Erro ao cadastrar caso, tente novamente');
+    }
+  }
+
   return (
     <div className="register-container">
       <div className="content">
@@ -25,11 +52,26 @@ export default function NewIncident() {
 
 
         <form>
-          <input placeholder="Título do caso "/>
-          <textarea type="email" placeholder="Descrição"/>
-          <input placeholder="Valor em Reais"/>
+          <input
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            placeholder="Título do caso "
+          />
+          <textarea
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+            type="email" placeholder="Descrição"
+          />
+          <input
+            value={value}
+            onChange={e => setValue(e.target.value)}
+            placeholder="Valor em Reais"
+          />
 
-          <button type="submit" className="button">
+          <button
+            onClick={handleNewIncident}
+            type="submit"
+            className="button">
             Cadastrar
           </button>
         </form>
