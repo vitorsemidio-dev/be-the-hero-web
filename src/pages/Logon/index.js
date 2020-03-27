cf2e71809b4067c859fec40dd7cb3fb861d00d89
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-03-26 21:01:18 -0300

    Adição ícone log-in e instalação lib

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
 
@@ -18,6 +19,7 @@ export default function Logon() {
           <button type="submit">Entrar</button>
 
           <a href="/register">
+            <FiLogIn size={16} color="#E02041"/>
             Não tenho cadastro
           </a>
         </form>
