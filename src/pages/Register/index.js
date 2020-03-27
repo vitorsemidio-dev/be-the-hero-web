4fde40eb6667b23d966e7d28db21529e0d18ee4a
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-03-26 21:51:05 -0300

    Estrutura Register

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
 
 import './styles.css'
 
+import logoImg from '../../assets/logo.svg';
+
 export default function Register() {
   return (
-    <div>
-      Register
+    <div className="register-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The Hero Logo"/>
+
+          <h1>Cadastro</h1>
+
+          <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem casos da sua ONG.</p>
+
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="#E02041"/>
+            Voltar para Logon
+          </Link>
+        </section>
+
+
+        <form>
+          <input placeholder="Nome da ONG"/>
+          <input type="email" placeholder="E-mail"/>
+          <input placeholder="WhatsApp"/>
+
+          <div className="input-group">
+            <input placeholder="Cidade"/>
+            <input placeholder="UF" style={{ width: 80 }}/>
+          </div>
+
+          <button type="submit" className="button">
+            Cadastrar
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
