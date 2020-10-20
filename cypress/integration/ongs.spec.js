84cbc8c6e6c7384a847e1bc865a80b85dc4bbee5
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-20 00:34:15 -0300

    Cy Test: Register com Page Object

--- cypress/integration/ongs.spec.js
+++ cypress/integration/ongs.spec.js
@@ -3,31 +3,13 @@
 /// <reference types="cypress" />
 
 import Logon from '../support/Pages/Logon';
+import Register from '../support/Pages/Register';
 
 describe('Ongs', () => {
-  it('Deve poder realizar cadastro', () => {
-    // Visitar página
-    cy.visit('http://localhost:3000/register');
-
-    // Seleção e preenchimento dos campos
-    cy.get('[data-cy=name]').type('Dogs queridos');
-    cy.get('[data-cy=email]').type('dogs@email.com');
-    cy.get('[data-cy=whatsapp]').type('21999999999');
-    cy.get('[data-cy=city]').type('Rio de Janeiro');
-    cy.get('[data-cy=uf]').type('RJ');
-
-    // Servidor
-    cy.server();
-    cy.route('POST', '**/ongs').as('postOng');
-
-    // Seleção botão submit e clique
-    cy.get('[data-cy=submit]').click();
-
-    cy.wait('@postOng').then((xhr) => {
-      expect(xhr.status).be.eq(200);
-      expect(xhr.response.body).has.property('id');
-      expect(xhr.response.body.id).is.not.null;
-    });
+  it.only('Deve poder realizar cadastro', () => {
+    Register.acessarCadastro();
+    Register.preencherCadastro();
+    Register.validarCadastroDeOngsComSucesso();
   });
 
   it('Deve poder realizar login', () => {
